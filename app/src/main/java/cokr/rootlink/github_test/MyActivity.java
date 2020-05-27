cc8bc79f3e02f79a22f946860096c9e85138b6bd
Junsuk Oh <a811219@gmail.com>
2020-05-27 20:39:22 +0900

    로그인 완성

--- app/src/main/java/cokr/rootlink/github_test/MyActivity.java
+++ app/src/main/java/cokr/rootlink/github_test/MyActivity.java
@@ -10,5 +10,7 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        // login
     }
 }
